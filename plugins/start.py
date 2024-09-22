f410b14c695fd1c7e5443b80c29351ce23d76696
Nammmekyarkhah2 <dummygacc60@gmail.com>
2024-09-22 10:36:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -82,7 +82,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
         k = await client.send_message(chat_id=message.from_user.id, 
-                                      text=f"<b><i>This File is deleting automatically in {file_auto_delete}. Forward in your Saved Messages..!</i></b>")
+                                      text=f"<b><i>This File is deleting automatically in {file_auto_delete}. You can not take screenshot and not able to forward..!</i></b>")
 
         # Schedule the file deletion
         asyncio.create_task(delete_files(codeflix_msgs, client, k))
